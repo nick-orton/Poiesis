dd47f6e590c25b245b941a09a85df1fbf34eb4e4
Nick Orton <orton.nick@gmail.com>
2011-09-07 18:58:28 -0400

    Atoms are Atomic

--- src/poiesis/forms.clj
+++ src/poiesis/forms.clj
@@ -1,1 +1,21 @@
 (ns poiesis.forms)
+
+(defprotocol Term
+  (atomic? [term]))
+
+(defprotocol Expression
+  (lambda? [expression])
+  (get-bound-vars [lambda])
+  (get-terms [expression]))
+
+(defprotocol Atom
+  (bound-by? [self expression])
+  (get-sym [self])
+  (eq? [self other]))
+
+
+(defn make-atom [sym]
+  (reify
+    Term
+    (atomic? [_] true)))
+
--- test/poiesis/test/forms.clj
+++ test/poiesis/test/forms.clj
@@ -2,5 +2,5 @@
   (:use [poiesis.forms])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest atoms-are-atomic 
+  (is (atomic? (make-atom "foo"))))

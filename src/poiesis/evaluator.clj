9fe9f487454ad38e306f01424b48cde8d2646148
Nick Orton <orton.nick@gmail.com>
2011-09-07 20:56:25 -0400

    Replace Free Occurances of a Variable

--- src/poiesis/evaluator.clj
+++ src/poiesis/evaluator.clj
@@ -1,1 +1,25 @@
-(ns poiesis.evaluator)
+(ns poiesis.evaluator
+  (:use poiesis.forms))
+
+
+(declare replace-free)
+
+(defn- replace-free*
+  [variable arg parent terms]
+  (if (empty? terms)
+    '()
+    (let [term (first terms)]
+         (if (atomic? term)
+             (if (and (not (bound-by? term parent))(eq? variable term))
+                 (cons arg (replace-free* variable arg parent (rest terms)))
+                 (cons term (replace-free* variable arg parent (rest terms))))
+             (cons (replace-free variable arg term) 
+                   (replace-free* variable arg parent (rest terms)))))))
+
+(defn replace-free
+  [variable arg term]
+  (if (atomic? term) 
+      term
+      (make-lambda 
+        (get-bound-vars term) 
+        (replace-free* variable arg term (get-terms term)))))
--- test/poiesis/test/evaluator.clj
+++ test/poiesis/test/evaluator.clj
@@ -1,6 +1,24 @@
 (ns poiesis.test.evaluator
   (:use [poiesis.evaluator])
+  (:use [poiesis.forms])
   (:use [clojure.test]))
 
-;TODO ME
+(def x (make-atom "x"))
+(def y (make-atom "y"))
+(def z (make-atom "z"))
+(def xy (make-lambda [] [x y]))
+(def lxy (make-lambda [x] [x y]))
+(def yy (make-lambda [] [y y]))
+(def xyz (make-lambda [] [x y z]))
+(def x-xy-z (make-lambda [] [x xy z]))
+(def x-lxy-z (make-lambda [] [x lxy z]))
 
+(deftest test-replace-free
+  (is (= [y y z] (get-terms (replace-free x y xyz ))))
+  (is (= [x x z] (get-terms (replace-free y x xyz ))))
+  (def replaced-x-xy-z (replace-free y x x-xy-z))
+  (defn terms-of-2nd-exp [exp] (get-terms (nth (get-terms exp) 1)))
+  (is (= [x x]   (terms-of-2nd-exp replaced-x-xy-z )))
+  (is (= [x y]   (terms-of-2nd-exp (replace-free x z x-lxy-z))))
+  (is (= [x z]   (terms-of-2nd-exp (replace-free y z x-lxy-z))))
+  (is (= [y y] (get-terms (replace-free x y xy )))))

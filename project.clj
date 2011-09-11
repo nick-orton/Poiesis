c495e3f2ebe477eb4d334c18b71d6b2734f097cd
Nick Orton <orton.nick@gmail.com>
2011-09-10 22:14:47 -0400

    can uberjar it up

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject poiesis "1.0.0-SNAPSHOT"
   :description "A dirt simple PL"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :main poiesis.core)
--- src/poiesis/core.clj
+++ src/poiesis/core.clj
@@ -2,8 +2,12 @@
   (:use poiesis.forms)
   (:use poiesis.lexer)
   (:use poiesis.parser)
-  (:use poiesis.evaluator))
+  (:use poiesis.evaluator)
+  (:gen-class))
 
 
 (defn compute [string]
   (str (evaluate (parse-l (lex string)))))
+
+(defn -main [& args]
+    (println "->" (compute (apply str args))))
